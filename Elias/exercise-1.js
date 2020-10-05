316447bdd70f2fbea7676a6de7389bc17aec05e4
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2020-10-05 13:17:11 +0200

    finished task

---
+++ Elias/exercise-1.js
@@ -0,0 +1,18 @@
+console.log("this is an exercise in console logging");
+
+var myVar = "this is the value.";
+
+console.log(myVar);
+
+var firstName = "Elias";
+var lastName = "Guderian";
+
+console.log(firstName, lastName);
+
+var userFirstName = "Tim";
+var userLastName = "Tin";
+var userCity = "Paris";
+var userAge = 33;
+var userMartialStatus = "married";
+console.log(userFirstName, userLastName, userCity, userAge, userMartialStatus);
+console.log(userFirstName + " " + userLastName + " is a " + userAge + " year old fireman who lives in " + userCity + " who is " + userMartialStatus + " to his wife Beth.");
