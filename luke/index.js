9e68fd1aae03f0938965839c926bf052e6ee87e7
Luke Dowling <lpdowling26@googlemail.com>
2020-10-05 13:33:01 +0200

    finished assignment

---
+++ luke/index.js
@@ -0,0 +1,33 @@
+// task 1
+console.log("this is an exercise in console logging");
+
+// task 2
+let str = "This is the value.";
+console.log(str);
+
+// task 3
+let firstName = "Luke";
+let lastName = "Dowling";
+console.log(firstName, lastName);
+
+// task 4
+let name = "John";
+let age = 22;
+let city = "Dublin";
+let job = "Programmer";
+let homeTown = "Bath";
+console.log(name, age, city, job, homeTown);
+
+// task 5
+console.log(
+  name +
+    " is " +
+    age +
+    " years old, living in " +
+    city +
+    " however his home town was " +
+    homeTown +
+    " where he studied to be a " +
+    job +
+    "."
+);
