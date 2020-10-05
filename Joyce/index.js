d389fd0eb17cc2d8a011476dc5e24e9a58d4d047
Joyce <puch1996@gmail.com>
2020-10-05 14:08:29 +0200

    modified

---
+++ Joyce/index.js
@@ -0,0 +1,32 @@
+// 1
+console.log("this is an exercise in console logging");
+
+// 2
+let dec = "This is the value.";
+console.log(dec);
+
+// 3
+let firstName = "Joyce";
+let lastName = "Sosaya";
+console.log(firstName, lastName);
+
+// 4
+let firstName1 = "Tony";
+let lastName1 = "Janathan";
+let city = "Bonn";
+let job = "web-developer";
+let age = 30;
+console.log(firstName1, lastName1, city, job, age);
+
+// 5
+console.log(
+  firstName1,
+  lastName1 +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    " who lives in " +
+    city +
+    "."
+);
