a5c61f9525078f9aa70ad9d955b8c2998df900d1
AneudyGarcia <67944953+AneudyGarcia@users.noreply.github.com>
2020-10-05 15:39:28 +0200

    Add files via upload

---
+++ lesson_1.js
@@ -0,0 +1,19 @@
+// 1
+console.log ("this is an exercise in console logging");
+// 2
+var name = "This is the value";
+console.log (name); 
+// 3
+let FirstName = "Aneudy";
+let LastName = "Garcia";
+console.log (FirstName, LastName)
+// 4
+let Name = "Carlos";
+let LastName ="Pinero"
+let City = "Berlin";
+let Job = "Stripper";
+let Age = "60";
+console.log (Name, LastName, City, Job, Age);
+//5
+console.log("I've met" + Name + LastName + "in" + City + "last week. He started working as a" + Job + "at age" +globalThis 
+//done
