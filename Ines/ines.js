4048a5ca2d5522f44618b351d1060596d78d1b0e
Ines <inesfcnogueira@gmail.com>
2020-10-05 14:03:28 +0200

    FC Ines

---
+++ Ines/ines.js
@@ -0,0 +1,13 @@
+console.log("this is an exercise in console logging");
+var variableName = "This is the value:";
+console.log(variableName);
+let FirstName = "Ines";
+let LastName = "Nogueira";
+console.log(FirstName, LastName);
+let city = "Berlin";
+let job = "Web Developer";
+let age = "28";
+console.log(
+  FirstName,
+  LastName + " lives in: " + city + " is a " + job + " and it's " + age
+);
