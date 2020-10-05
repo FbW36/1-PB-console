4f861fe315b9517566ccd67941d1383f5862e1f7
Tony Tanaka <tony.h.tanaka@gmail.com>
2020-10-05 13:52:40 +0200

    finished exercise

--- tony/index.js
+++ tony/index.js
@@ -1,1 +1,26 @@
-console.log("first js code.");
+console.log("this is an exercise in console logging");
+
+let value1 = "This is the value.";
+console.log(value1);
+
+let firstName = "Tony";
+let lastName = "Tanaka";
+console.log(firstName, lastName);
+
+let name = "John";
+let surname = "Doe";
+let city = "Dusseldorf";
+let job = "Program Manager";
+let country = "Germany";
+
+console.log(
+  name +
+    " " +
+    surname +
+    " lives in " +
+    city +
+    " and works a " +
+    job +
+    " for companies in all " +
+    country
+);
