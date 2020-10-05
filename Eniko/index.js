8b22cf88529a53c582a08582cc7f8609e2d9a40e
Enikő Veszelka <encsooo@gmail.com>
2020-10-05 13:55:52 +0200

    exercise done

---
+++ Eniko/index.js
@@ -0,0 +1,15 @@
+"This is the first exercise"
+
+var firstVar = "This is the value.";
+console.log(firstVar);
+
+var firstName = 'Enikö';
+var lastName = 'Veszelka';
+console.log(firstName, lastName);
+
+let age = 31;
+let city = 'Berlin';
+let inBerlin = 10;
+let pet = 'cat';
+let zodiac = 'aquarius';
+console.log("Hi! I am " + firstName, lastName + " and I am from " + city + ". I am living here for " + inBerlin + " years. I have a " + pet + " and I am an " + zodiac + ".");
