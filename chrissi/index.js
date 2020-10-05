467ad3ebffaf73e3ab73444c7113aaf2ff437893
Chrissi <cmette.online@googlemail.com>
2020-10-05 13:42:51 +0200

    add tasks

---
+++ chrissi/index.js
@@ -0,0 +1,37 @@
+// task 1
+console.log("this is an exercise in console logging");
+
+// task 2
+let var1 = "This is the value.";
+
+console.log(var1);
+
+//task 3
+
+let firstName = "Chrissi";
+let lastName = "Mette";
+
+console.log(firstName, lastName);
+
+// task 4
+
+let name = "Uschi ";
+let surname = "Wagner";
+let age = 45;
+let city = "fantasyland";
+let job = "stone cutter";
+
+console.log(name, surname, age, city, job);
+
+// task 5
+
+console.log(
+  name +
+    surname +
+    " in the age of " +
+    age +
+    " is from " +
+    city +
+    " and works as a " +
+    job
+);
