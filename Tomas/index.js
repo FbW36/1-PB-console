4d13fc5a89344633351d28a08cd262a2289e373a
Tomas <eliz.tomas23@gmail.com>
2020-10-05 13:41:55 +0200

    initial commit

---
+++ Tomas/index.js
@@ -0,0 +1,16 @@
+console.log('this is an exercise in console logging')
+let myLet = 'This is the value'
+console.log(myLet)
+
+let myFirstName = 'Tomas'
+let myLastName = 'Eliz'
+console.log(myFirstName, myLastName)
+
+let firstName = 'Dave'
+let lastName = 'Lafleur'
+let city = 'Portland'
+let age = '101'
+let occupation = 'watchman'
+console.log(firstName, lastName, city, age, occupation)
+
+console.log(firstName, lastName + ' is a ' + age + ' old ' + occupation + ' from ' + city + '.')
