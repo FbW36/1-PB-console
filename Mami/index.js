483992966a016e7aa7d50ac5f3b0eec2c0f92bb2
carveler <3heyaberlin@gmail.com>
2020-10-05 13:50:27 +0200

    initial commit

---
+++ Mami/index.js
@@ -0,0 +1,29 @@
+console.log("this is an exercise in console logging");
+var myvar = "This is the value";
+
+var firstName = "Mami";
+var lastName = "Kurokawa";
+console.log(firstName, lastName);
+
+var myName = "Mami Kurokawa";
+var myCity = "Berlin";
+var myJob = "none";
+var myHeight = "155cm";
+var myBloodtype = "A";
+console.log(myName, myCity, myJob, myHeight, myBloodtype);
+console.log(
+  "my name is " +
+    myName +
+    " " +
+    "i live in " +
+    myCity +
+    " " +
+    "my job is " +
+    myJob +
+    " " +
+    "my height is " +
+    myHeight +
+    " " +
+    "my bloodtype is " +
+    myBloodtype
+);
