de1e13db8bb9092280d1aa9a859cf9beb98a7b32
Feruj Ahammed <ferujahammed@gmail.com>
2020-10-08 14:38:18 +0200

    new

---
+++ Feruj/exercise1.js
@@ -0,0 +1,19 @@
+console.log("this is an exercise in console logging")
+//
+let myvalu = "This is the value."
+console.log(myvalu)
+//
+
+let firstName = 'Feruj'
+let lastName = 'Ahammed'
+console.log(firstName, lastName)
+//
+let firstName2 = 'Tom'
+let lastName2 = 'and Jerry'
+let age = 30
+let curntCity = 'Berlin'
+let marstatus = "Married"
+console.log(firstName2, lastName2, age, curntCity, marstatus);
+
+//
+console.log(firstName + " " + lastName + " is " + age + " years old" + "," + " living in berlin. He is " + marstatus + " to Ena")
