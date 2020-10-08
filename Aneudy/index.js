51e00502bb5e7ab9794dd4564eafd729bf5817dd
Aneudy <mailaneudygarcia@gmail.com>
2020-10-08 15:31:10 +0200

    ok

---
+++ Aneudy/index.js
@@ -0,0 +1,19 @@
+// 1
+console.log ("This is an exercise in console logging.");
+// 2
+var name = "This is the value.";
+console.log (name); 
+// 3
+let FirstName = "Aneudy";
+let LastName = "Garcia";
+console.log (FirstName, LastName)
+// 4
+let Name = "Carlos";
+let SecondName = "Pinero";
+let City = "Berlin";
+let Job = "DJ";
+let Age = "30";
+console.log (Name, SecondName, City, Job, Age);
+//5
+console.log(`I met ${Name}${SecondName}when he was ${Age}years old in ${City}. He was a great ${Job}.`);
+// done
