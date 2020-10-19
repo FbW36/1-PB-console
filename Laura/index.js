37b8c79252b5699c80c88252ab679838c61b3411
limononoil <63930000+limononoil@users.noreply.github.com>
2020-10-19 21:03:09 +0200

    sorry for the delay

---
+++ Laura/index.js
@@ -0,0 +1,27 @@
+console.log("this is an exercise in console logging");
+
+var name = "This is a variable";
+console.log(name);
+
+var firstName = "Laura";
+var secondName = "Pacis";
+console.log(firstName, secondName);
+
+var firstName = "Ana";
+var secondName = "Maria";
+var age = 50;
+var city = "Berlin";
+var job = "Lawyer";
+console.log(firstName, secondName, age, city, job);
+
+console.log(
+  firstName +
+    " " +
+    secondName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    " who lives in " +
+    city
+);
